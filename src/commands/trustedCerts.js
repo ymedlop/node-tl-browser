9ec756747c7c4d2879f9ef66e329216eb9f97ac9
Yeray Medina López <ymedlop@gmail.com>
2020-05-04 10:35:33 +0200

    Fix issue with CERT name

--- src/commands/trustedCerts.js
+++ src/commands/trustedCerts.js
@@ -2,6 +2,10 @@ const fs = require("fs-extra");
 const exec = require('child_process').exec;
 const dataMock = require("../assets/mocks/tl/DE.json");
 
+function parseName(name) {
+  return name.replace(/\\/g, "_").replace(/\//g, "_").replace(" ", "_").toLowerCase();
+}
+
 function writeOutput(cc, data, serviceFilter, exported = false) {
   const sp = data.tl.serviceProviders;
   const results = sp.reduce((items, item) => {
@@ -46,8 +50,8 @@ function writeOutput(cc, data, serviceFilter, exported = false) {
               // TODO: Parameter chek expiration by default always
               if (cert.certEncoded && cert.certAfter >= new Date().getTime()) {
                 const kind = serviceFilter ? `-${serviceFilter}` : "";
-                const entityName = item.name.replace(" ", "_").toLowerCase();
-                const certName = cert.certSubjectShortName.replace(/\s/g, "_").toLowerCase();
+                const entityName = parseName(item.name);
+                const certName = parseName(cert.certSubjectShortName);
                 const name = `${distFolder}/${cc}${kind}_${entityName}.${certName}.crt`;
                 const content = `-----BEGIN CERTIFICATE-----\r\n${cert.certEncoded}\r\n-----END CERTIFICATE-----`;
                 writeFile(name, content);
@@ -67,7 +71,7 @@ function writeFile(name, output) {
   fs.writeFile(name, output, err => {
     if (err) return console.error(err);
     console.log("Writing file was OK: ", name);
-    const command = `openssl x509 -in ${name} -outform PEM -out ${name}.pem`;
+    const command = `openssl x509 -in "${name}" -outform PEM -out "${name}.pem"`;
      setTimeout(() => {
         exec(command, (err, stdout, stderr) => {
 	   if (err) console.error(err);
